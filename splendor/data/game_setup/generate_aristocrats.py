b944a45fb23262363248f921f41e43f8c0d1a358
Tesla2000 <fratajczak124@gmail.com>
2023-10-18 21:57:15 +0200

    Added Splendor-python as submodule

--- splendor/data/game_setup/generate_aristocrats.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-from splendor.data.Aristocrat import Aristocrat
-
-
-def generate_aristocrats() -> list[Aristocrat]:
-    aristocrat_data = Path("splendor/aristocrats.csv").read_text().splitlines()[1:]
-    return list(map(Aristocrat.from_text, aristocrat_data))
