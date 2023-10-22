c32ab13418c7df0dc5aa80cba2547da97c2ad605
Tesla2000 <fratajczak124@gmail.com>
2023-10-22 18:10:29 +0200

    Successfully running game using MCTS

--- GameLogic/play_a_game.py
+++ GameLogic/play_a_game.py
@@ -2,10 +2,7 @@ from collections import defaultdict
 
 from sklearn.tree import DecisionTreeClassifier
 
-from GameLogic.choose_move import choose_move
 from PySplendor.Game import Game
-from alpha_trainer.exceptions.GameFinishedException import GameFinishedException
-from alpha_trainer.exceptions.NoPossibleMoveException import NoPossibleMoveException
 
 
 def play_a_game(
@@ -18,22 +15,14 @@ def play_a_game(
     states = defaultdict(list)
     while True:
         current_player = game.current_player
-        state = game.get_state()
-        try:
-            move, move_index = choose_move(
-                game, state, game.all_moves, model, model_decision_weight
-            )
-        except NoPossibleMoveException:
-            winner_id = id(game.players[-1])
-            x += states[winner_id]
-            y += len(states[winner_id]) * [1]
+        game = game.get_best_state(10)
+        if game.is_terminal():
+            for player in game.players:
+                result = game.get_result(player).value
+                player_id = player.id
+                x += states[player_id]
+                y += len(states[player_id]) * [result]
+            print(len(states[game.current_player.id]))
             break
-        states[id(current_player)].append(state + [move_index])
-        move.perform(game)
-        try:
-            game.next_turn()
-        except GameFinishedException as e:
-            winner_id = id(e.args[0])
-            x += states[winner_id]
-            y += len(states[winner_id]) * [1]
-            break
+        states[current_player.id].append(game.get_state())
+        game.next_turn()
